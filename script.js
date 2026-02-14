c623fcaeaaff148a2eaae83a91b58b05c591b1e1
NuclearGod124 <conreese10@gmail.com>
2026-02-14 18:20:05 -0500

    test2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "a-2.4.1";
+const APP_VERSION = "a-2.4.2";
 let swRegistration = null;
 let deferredPrompt = null;
 
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "biohazard-cache-a2.4.1";
+const CACHE_NAME = "biohazard-cache-a2.4.2";
 
 const FILES_TO_CACHE = [
     "./",
